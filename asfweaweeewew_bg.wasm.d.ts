57a52371480987099913e12fec0be0f4eec4a19e
ComputersAreCool <computersarecooltm@gmail.com>
2025-06-12 07:31:08 -0400

    Add Exising File

---
+++ asfweaweeewew_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure1183_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb16960eee124d16c: (a: number, b: number, c: number) => void;
+export const closure7357_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hca89cc0de6164594: (a: number, b: number) => void;
+export const closure7362_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0e0bbbdf562a1f37: (a: number, b: number) => void;
+export const closure122393_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
